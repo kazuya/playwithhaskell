4fb22d8dc7ae2c6029572a327ac34256fc7708d6
Kazuya Sakakihara <kazuya@gmail.com>
2008-08-23 16:54:33 +0900

    Added custom show method.

--- scheme48/simple_parser.hs
+++ scheme48/simple_parser.hs
@@ -14,7 +14,23 @@ data LispVal = Atom String
              | Bool Bool
              | Character Char
              | Vector (Array Int LispVal)
-               deriving Show
+--               deriving Show
+
+instance Show LispVal where show = showVal
+
+showVal :: LispVal -> String
+showVal (String contents) = "\"" ++ contents ++ "\""
+showVal (Atom name) = name
+showVal (Number contents) = show contents
+showVal (Float contents) = show contents
+showVal (Bool True) = "#t"
+showVal (Bool False) = "#f"
+showVal (List contents) = "(" ++ unwordsList contents ++ ")"
+showVal (DottedList head tail) = "(" ++ unwordsList head ++ " . " ++ showVal tail ++ ")"
+showVal contents = show contents
+
+unwordsList :: [LispVal] -> String
+unwordsList = unwords . map showVal
 
 parseVector :: Parser LispVal
 parseVector = do
