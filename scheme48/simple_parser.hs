6aaf6c85f4b56e0241d2027b8dde58ae4b0521de
Kazuya Sakakihara <kazuya@gmail.com>
2008-08-24 20:52:12 +0900

    Added simple-minded predicates for string, symbol, and number.

--- scheme48/simple_parser.hs
+++ scheme48/simple_parser.hs
@@ -16,6 +16,58 @@ data LispVal = Atom String
              | Vector (Array Int LispVal)
 --               deriving Show
 
+eval :: LispVal -> LispVal
+eval val@(String _) = val
+eval val@(Number _) = val
+eval val@(Bool _) = val
+eval (List [Atom "quote", val]) = val
+eval (List (Atom fun : args)) = apply fun $ map eval args
+
+apply :: String -> [LispVal] -> LispVal
+apply fun args = maybe (Bool False) ($ args) $ lookup fun primitives
+
+primitives :: [(String, [LispVal] -> LispVal)]
+primitives = [
+              ("+", numericBinOp (+)),
+              ("-", numericBinOp (-)),              
+              ("*", numericBinOp (*)),
+              ("/", numericBinOp div),              
+              ("mod", numericBinOp mod),              
+              ("quotient", numericBinOp quot),             
+              ("remainder", numericBinOp rem),
+              ("string?", isString),
+              ("symbol?", isSymbol),
+              ("number?", isNumber)
+             ]
+
+isString :: [LispVal] -> LispVal
+isString ((String _):_) = Bool True
+isString _ = Bool False
+
+isNumber :: [LispVal] -> LispVal
+isNumber ((Number _):_) = Bool True
+isNumber _ = Bool False
+
+isSymbol :: [LispVal] -> LispVal
+isSymbol ((Atom _):_) = Bool True
+isSymbol _ = Bool False
+
+
+
+--predicateOp :: 
+
+numericBinOp :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
+numericBinOp op params = Number $ foldl1 op $ map unpackNum params
+
+unpackNum :: LispVal -> Integer
+unpackNum (Number n) = n
+unpackNum (String n) = let parsed = reads n in
+                       if null parsed
+                          then 0
+                          else fst $ parsed !! 0
+unpackNum (List [n]) = unpackNum n
+unpackNum _ = 0
+
 instance Show LispVal where show = showVal
 
 showVal :: LispVal -> String
@@ -166,11 +218,11 @@ symbol = oneOf "!$#%&|*+-/:<=>?@^_~"
 spaces :: Parser ()
 spaces = skipMany1 space
 
-readExpr :: String -> String
+readExpr :: String -> LispVal
 readExpr input = case parse parseExpr "lisp" input of
-                   Left err -> "No match: " ++ show err
-                   Right val -> "Found value: " ++ show val
+                   Left err -> String $ "No match: " ++ show err
+                   Right val -> val
 
 main :: IO ()
 main = do args <- getArgs
-          putStrLn $ readExpr $ args !! 0
+          putStrLn $ show $ eval $ readExpr $ args !! 0
