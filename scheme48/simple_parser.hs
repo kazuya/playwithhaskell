98e9ee0383df78640deca56875a5d31bdfd261b4
Kazuya Sakakihara <kazuya@gmail.com>
2008-08-29 21:25:26 +0900

    Added 'if' construct.

--- scheme48/simple_parser.hs
+++ scheme48/simple_parser.hs
@@ -21,6 +21,11 @@ eval :: LispVal -> ThrowsError LispVal
 eval val@(String _) = return val
 eval val@(Number _) = return val
 eval val@(Bool _) = return val
+eval (List [Atom "if", pred, conseq, alt]) =
+    do result <- eval pred
+       case result of
+         Bool False -> eval alt
+         otherwise -> eval conseq
 eval (List [Atom "quote", val]) = return val
 eval (List (Atom fun : args)) = mapM eval args >>= apply fun
 eval badForm = throwError $ BadSpecialForm "Unrecognized special form" badForm
